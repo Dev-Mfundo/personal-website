da9ae67efbeea5d79c221eccb2a149c9e3e1dc3b
Aaron Jiane <devmfundo@gmail.com>
2025-04-07 18:51:30 +0200

    saving progress: form bug fixed, and polished

--- js/script.js
+++ js/script.js
@@ -17,3 +17,15 @@ $(document).ready(function() {
 const year = new Date().getFullYear()
 const fyear = document.getElementById('time')
 fyear.textContent = `© ${year} Aaron Jiane`
+
+const emailAlert = document.getElementById("alert-user")
+const formSubmit = document.getElementsByClassName("submit-btn")
+
+Array.from(formSubmit).forEach((button) => {
+    button.addEventListener('click', () => {
+        emailAlert.textContent = "...Opening your email client to send the message.";
+        setTimeout(()=>{
+            emailAlert.textContent ="Say something nice.";
+        },5000)
+    })
+})
