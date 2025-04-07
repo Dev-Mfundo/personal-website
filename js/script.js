2b3de20e61f62bf1a947ed360c39c9d2035453b0
Aaron Jiane <devmfundo@gmail.com>
2025-04-07 20:33:45 +0200

    saving progress: clean up

--- js/script.js
+++ js/script.js
@@ -2,18 +2,6 @@ const year = new Date().getFullYear()
 const fyear = document.getElementById('time')
 fyear.textContent = `© ${year} Aaron Jiane`
 
-const emailAlert = document.getElementById("alert-user")
-const formSubmit = document.getElementsByClassName("submit-btn")
-
-Array.from(formSubmit).forEach((button)=>{
-    button.addEventListener('click', ()=>{
-        emailAlert.textContent = "...Opening your email client to send the message.";
-        setTimeout(()=>{
-            emailAlert.textContent ="Say something nice.";
-        },5000)
-    })
-})
-
 const scrollUpBtn = document.getElementById('scroll-up-btn');
 
 window.addEventListener('scroll',()=>{
